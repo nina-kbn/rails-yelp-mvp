6f9643d737f04baa622987698861c1da7b3c1113
Nina Laouadi <nina.laouadi0@gmail.com>
2023-11-29 15:50:28 +0100

    mini yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "03 20 44 55 66", category: "french"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "04 13 44 55 66", category: "italian"}
+sushy_house =  {name: "Sushy House", address: "A bla bla", phone_number: "04 13 11 22 33", category: "japanese"}
+chinese_house =  {name: "Chinese House", address: "56A toto", phone_number: "04 13 99 88 77", category: "chinese"}
+baraque_a_frite =  {name: "Baraque a frite", address: "5 titi", phone_number: "03 20 22 33 44", category: "belgian"}
+
+[dishoom, pizza_east, sushy_house, chinese_house, baraque_a_frite].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
